a05bc741c90f718479f398d9db4eda8df3d84e54
Richard Clark <70902385+15014370uhi@users.noreply.github.com>
2021-01-23 14:34:16 +0000

    Create basic react-app

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require ('express');
 const mongoose = require ('mongoose');
 const bodyParser = require ('body-parser');
 
+// File location for itmes endpoint
+const items = require ('./routes/items');
+
 const app = express ();
 
 // BodyParser Middleware
@@ -15,6 +18,9 @@ mongoose
   .connect (db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then (() => console.log ('Mongo DB is connected'))
   .catch (err => console.log (err));
+
+  // Set Route Endpoint items - passing items const to show location of endpoint files
+  app.use('/items', items);
 
   const port = process.env.PORT || 5000;
 
