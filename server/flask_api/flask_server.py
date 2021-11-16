de7c22d41f9b62d98895fc03537c6dd325be7387
Richard Clark <70902385+15014370uhi@users.noreply.github.com>
2021-11-16 13:53:20 +0000

    Reintegrated flask server

--- server/flask_api/flask_server.py
+++ server/flask_api/flask_server.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
-import json
-#import os
 from myLogisticRegression import getPredictions 
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -22,12 +21,9 @@ def runPredictionModel():
 
     # call machine learning function with data
     result = getPredictions(month, year, lat, lon, sector)   
-    results_JSON = json.dumps(result)
-   
-   # print("results_JSON: ", results_JSON)
+    results_JSON = json.dumps(result)   
     
     return results_JSON
 
 if __name__ == "__main__":
-   #app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))   
    app.run(port=5000)
